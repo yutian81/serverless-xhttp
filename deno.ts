dfb18f6938fd765bd533d4f8647ca1ef6aa828af
eooce <142894633+eooce@users.noreply.github.com>
2025-03-21 17:37:35 +0800

    deno.ts

--- deno.ts
+++ deno.ts
@@ -1,11 +1,11 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const UUID: string = Deno.env.get("UUID") || "b70a3a2f-5eae-4311-ad37-29a30536e59b";
-const SUB_PATH: string = Deno.env.get("SUB_PATH") || "sub"; 
-const XPATH: string = Deno.env.get("XPATH") || "xhttp";      
-const DOMAIN: string = Deno.env.get("DOMAIN") || "";   // deno分配的域名必填，不带https://前缀，例如：xxxx.deno.dev      
-const NAME: string = Deno.env.get("NAME") || "Deno";        
-const PORT: number = parseInt(Deno.env.get("PORT") || "3000");
+const SUB_PATH: string = Deno.env.get("SUB_PATH") || "sub";  //获取订阅路径
+const XPATH: string = Deno.env.get("XPATH") || "xhttp";      // 节点路径
+const DOMAIN: string = Deno.env.get("DOMAIN") || "";         // deno分配的域名必填，不带https://前缀，例如：xxxx.deno.dev      
+const NAME: string = Deno.env.get("NAME") || "Deno";         // 名称
+const PORT: number = parseInt(Deno.env.get("PORT") || "3000"); 
 
 interface Settings {
   UUID: string;
